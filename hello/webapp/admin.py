9b73f6c94da16019abb9c2603e766125e31b5958
Aiganysh <aiganyshx@gmail.com>
2021-02-26 20:31:18 +0600

    added style.css for display style on page

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from webapp.models import List
 # Register your models here.
+
+# class ListAdmin(admin.ModelAdmin):
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'describe', 'date_of_completion']
+    list_filter = ['describe']
+    search_fields = ['status', 'describe']
+    fields = ['id', 'status', 'describe', 'date_of_completion']
+    readonly_fields = ['date_of_completion', 'id']
+
+
+admin.site.register(List, ListAdmin)
